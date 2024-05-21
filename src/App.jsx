960c190966c9d23d33ade5431882f845ee4068c5
Michael <michael.boitmann@gmail.com>
2024-05-21 23:00:43 +1200

    update index.html, added components/nav folder and file

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+import Nav from './components/Nav';
 
 const App = () => {
   return (
     <main className="relative">
-      Nav
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
-        Hero
+        <Hero />
       </section>
       <section className="padding">
-        Popular Products
+        <PopularProducts />
       </section>
       <section className="padding">
-        SuperQuality
+        <SuperQuality />
       </section>
       <section className="padding-x py-10">
-        Services
+        <Services />
       </section>
       <section className="padding">
-        SpecialOffer
+        <SpecialOffer />
       </section>
       <section className="bg-pale-blue padding">
-        CustomerReviews
+        <CustomerReviews />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        Subscribe
+        <Subscribe />
       </section>
       <section className="bg-black padding-x padding-t pb-8 padding font-white">
-        Footer
+        <Footer />
       </section>
     </main>
   )

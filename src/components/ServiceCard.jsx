9de6a494de154dd81caac58e1506112562e87757
Michael <michael.boitmann@gmail.com>
2024-05-27 18:31:06 +1200

    completed special offer section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,13 +4,19 @@ const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
     <section className='flex-1 sm:w-[350px] sm:min-w-[350px]
     w-full rounded-[20px] shadow-3xl px-10 py-16'>
-      <div className='w-11 h-11 justify-center items-center
-      bg-coral-red rounded-full'>
-        <img src={imgURL} alt={label} width={24} height={24} />
+      <div className='w-10 h-10 justify-center items-center
+      bg-coral-red rounded-full pl-2 pt-2'>
+        <img 
+          src={imgURL}
+          alt={label}
+          width={24} 
+          height={24} />
       </div>
       <h3 className='mt-5 font-palanquin text-3xl leading-normal
-      font-bold'>{label}</h3>
-      <p className='mt-3 break-words font-montserrattext-lg 
+      font-bold'
+      >{label}
+      </h3>
+      <p className='mt-3 break-words font-montserrat text-lg 
       leading-normal text-slate-gray'>{subtext}</p>
     </section>
   )
